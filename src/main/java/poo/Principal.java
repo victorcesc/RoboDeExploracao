974809ce0a0a0851d90eadf8afa723fb5f220234
victor cesc <victorcesconetto@gmail.com>
2018-03-17 16:30:50 -0300

    robo implementado, falta o main

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -3,5 +3,11 @@ package poo;
 public class Principal {
     public static void main(String[] args) {
 
+        Robo r = new Robo();
+
+        System.out.println(r.MostraLoc());
+        r.mover("M");
+        System.out.println(r.MostraLoc());
+
     }
 }
--- src/main/java/poo/Robo.java
+++ src/main/java/poo/Robo.java
@@ -3,18 +3,18 @@ package poo;
 public class Robo {
 
     private int locx,locy;
-    public int areax,areay;
+    private int areax,areay;
     private String frente;
-    private String locatual;
 
 
-    public Robo(int locx, int locy, int areax, int areay, String frente, String locatual) {
+
+    public Robo(int locx, int locy, int areax, int areay, String frente) {
         this.locx = locx;
         this.locy = locy;
         this.areax = areax;
         this.areay = areay;
         this.frente = frente;
-        this.locatual = locatual;
+
     }
 
     public Robo() {
@@ -23,45 +23,88 @@ public class Robo {
         this.areax = 10;
         this.areay = 10;
         this.frente = "N";
-        this.locatual = locx+" "+locy+" "+"N";
+        //this.localatual = locx+" "+locy+" "+frente;
 
     }
 
+    public void Area(int x,int y){
+        this.areay=y;
+        this.areax=x;
+    }
 
-    public String mover(String S){
-        if(S == "M"){
-            if(this.frente == "N"){
-                if(this.locy<this.areay) {
-                    this.locy++;
-                }else{
-                    return "Voce esta no limite da Area de Exploracao!";
+    public String MostraLoc(){
+        return locx+" "+locy+" "+frente;
+    }
+
+
+
+    public String warnLimits(String s){
+        String x="";
+        if(s=="L.Exp") x =  "Voce esta no limite da Area de Exploracao!";
+        if(s=="ErroMover") x = "Comando invalido de movimentacao!";
+        return x;
+    }
+
+    public void mover(String S) {
+        if (S == "M" || S == "E" || S == "D") {
+            if (S == "M") {
+                if (this.frente == "N") {
+                    if (this.locy < this.areay) {
+                        this.locy++;
+                    } else {
+                        System.out.println(warnLimits("L.Exp"));
+                    }
+                }
+                if (this.frente == "S") {
+                    if (this.locy > this.areay) {
+                        this.locy--;
+                    } else System.out.println(warnLimits("L.Exp"));
+                }
+                if (this.frente == "L") {
+                    if (this.locx < this.areay) {
+                        this.locx++;
+                    } else System.out.println(warnLimits("L.Exp"));
+                }
+                if (this.frente == "O") {
+                    if (this.locx > this.areay) {
+                        this.locx--;
+                    } else System.out.println(warnLimits("L.Exp"));
                 }
             }
-            if(this.frente=="S"){
-                if(this.locy>this.areay) {
-                    this.locy--;
-                }else return "Voce esta no limite da Area de Exploracao!";
-            }
-            if(this.frente=="L"){
-                if(this.locx<this.areay) {
-                    this.locx++;
-                }else return "Voce esta no limite da Area de Exploracao!";
-            }
-            if(this.frente=="O"){
-                if(this.locx>this.areay) {
-                    this.locx--;
-                }else return "Voce esta no limite da Area de Exploracao!"
-            }
-        }
-        if(S=="E"){
-            if(this.frente=="N"){
-                this.frente = "O";
-            }
-        }
+            if (S == "E") {
+                if (this.frente == "N") {
+                    this.frente = "O";
+                }
+                if (this.frente == "S") {
+                    this.frente = "L";
+                }
+                if (this.frente == "O") {
+                    this.frente = "S";
+                }
+                if (this.frente == "L") {
+                    this.frente = "N";
+                }
 
+            }
+            if (S == "D") {
+                if (this.frente == "N") {
+                    this.frente = "L";
+                }
+                if (this.frente == "S") {
+                    this.frente = "O";
+                }
+                if (this.frente == "O") {
+                    this.frente = "N";
+                }
+                if (this.frente == "L") {
+                    this.frente = "S";
+                }
+            }
+
+        }else{
+            System.out.println(warnLimits("ErroMover"));
+        }
     }
-
-
 
 
 }
